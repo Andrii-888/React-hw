452a49162c675344233cc060e78f9b9228a1f874
Andrii-888 <122465780+Andrii-888@users.noreply.github.com>
2023-08-24 13:17:51 +0200

    add style

--- src/App.jsx
+++ src/App.jsx
@@ -16,13 +16,14 @@ function App() {
       <Schools schools={objSchool} />
       <Sport />
       <Family>
-      <h2 style={{ color: "green", backgroundColor: "orange" }}>My family</h2>
-      <ul>
-      <li>wife: Maria , born 1991 01.20</li>
-      <li>older daughter: Alexsandra , born 2010 05.11</li>
-      <li>younger daughter: Yeva , born 2017 09.10</li>
-      
-      </ul>
+        <h2 style={{ color: "green", backgroundColor: "orange" }}>My family</h2>
+        <ul>
+          <li>wife: Maria , born 1991 01.20</li>
+          <li>older daughter: Alexsandra , born 2010 05.11</li>
+          <li style={{ color: "green", backgroundColor: "orange" }}>
+            younger daughter: Yeva , born 2017 09.10
+          </li>
+        </ul>
       </Family>
     </div>
   );
